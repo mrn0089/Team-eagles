dcd3ed6551d4fba87d4c0769e2558ea9387d271f
mrn0089 <81244517+mrn0089@users.noreply.github.com>
2021-07-17 18:05:22 -0500

    Update movie.cpp

--- movie.cpp
+++ movie.cpp
@@ -3,6 +3,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void Movie::printMenu(){
+	int choice = 1;
+	bool stop;
+	do{
+	cout<<"________________________________________"<<endl;
+	cout<<"Please select an option"<<endl;
+	cout<<"1.Add movie."<<endl;//complete
+	cout<<"2.Remove movie."<<endl;//complete
+	cout<<"3.Edit a movie."<<endl;
+	cout<<"4.Display movie list"<<endl;//complete
+	cout<<"Press anything to exit."<<endl;
+	cout<<"________________________________________"<<endl;	
+	cin>>choice;
+	if(choice == 1){addMovie();}
+	else if (choice == 2){removeMovie();}
+	else if (choice == 3){editMovie();}
+	else if (choice == 4){printMovieList();}
+    }
+	else return;
+	} while(choice == 1 ||choice == 2 ||choice == 3 ||choice == 4);
+
+
+
+}
+
 void Movie::addMovie()
 {
     ifstream inFile("Movie.txt"); //open movies text file to read
@@ -159,4 +184,128 @@ void Movie::removeMovie()
     movie.clear();
     
     return;
+}
+
+//edit an existing movie
+void Movie::editMovie(){
+	ifstream myfile("Movie.txt"); //Opeining the file
+	vector<Movie> movie;	
+	string line;
+	int count=0;	
+	string title;
+	string time;
+	string genre;
+	string rating;
+	float duration;
+	int choice;
+	bool isfound = 0;
+	
+	cout<<"Enter Title of Movie to be Edited"<<endl;
+	cin>>title;
+	cout<<"Enter Time:"<<endl;
+	cin>>time;
+	
+	//loop through .txt file
+	while(getline(myfile,line)){
+		
+	staff.push_back(Movie());//Add an element to the vector
+	
+	stringstream ss(line);
+	
+    //start reading from file
+	getline(ss,title, ','); 
+	movie[count].setMovTitle(title);
+	
+	getline(ss,time, ',');
+    movie[count].setMovTime(time);
+	
+	getline(ss,rating, ',');
+	movie[count].setMovRating(rating);
+	
+	getline(ss,genre, ',');
+	movie[count].setMovGenre(genre);
+    
+    getline(ss,duration, ',');
+	movie[count].setMovDuration(duration)
+	
+	count++;//counts the loop iteration
+	}
+	for(int i=0; i<staff.size(); i++){
+		//Turns the vector into a temp variable for the loops.
+		string tempfirst;
+		string templast;
+		
+		tempfirst = movie[i].movTitle;
+		templast = movie[i].movTime;
+
+		if(tempfirst == title && templast == time){
+			//If valid movie found, remember the element location
+			cout<<"Movie found"<<endl;
+			cout<<"Select what you want to edit"<<endl;
+			cout<<"1.Time"<<endl;
+			cout<<"2.Rating"<<endl;
+			cout<<"3.Genre"<<endl;
+            cout<<"4.Duration"<<endl;
+			cin>>choice;
+			if(choice == 1){
+				cout<<"Enter new time"<<endl;
+				cin>>time;
+				movie[i].movTime = time;
+
+			}
+			else if (choice == 2){
+				cout<<"Enter new rating"<<endl;
+				cin>>rating;
+				movie[i].movRating = rating;
+				
+			}
+            else if(choice == 3){
+                cout<<"Enter new genre"<<endl;
+                cin>>genre;
+                movie[i].movGenre = genre;
+            }
+            else if(choice == 4){
+                cout<<"Enter new duration"<<endl;
+                cin>>duration;
+                movie[i].movDuration = duration;
+            }
+			else{
+				cout<<"Movie not found";
+				return;
+				}		
+		}
+	}
+	
+		ofstream myfile("Movie.txt");
+		for(int i=0; i<movie.size(); i++){
+			myfile<<movie[i].movTitle<<","<<movie[i].moveTime<<","<<movie[i].movRating<<","<<movie[i].movGenre<<","<<movie[i].movDuration<<endl;
+		}
+		
+		myfile.close();
+		movie.clear();	
+		return;
+}
+
+//display existing movies
+void Movie::displayMovieList(){
+	ifstream myfile("Movie.txt");
+    
+    string line;
+    string title;
+	string time;
+	string genre;
+	string rating;
+	float duration;
+	
+	while(getline(myfile,line)){//Reads the text document
+		stringstream ss(line);
+		getline(ss,title, ',');
+		getline(ss,time, ',');
+		getline(ss,rating, ',');
+		getline(ss,genre, ',');
+        getline(ss, duration, ',');
+		cout<<title<<" "<<time<< " "<<rating< " "<<genre<<" "<<duration<<endl;
+	}
+	myfile.close();
+	return;
 }
